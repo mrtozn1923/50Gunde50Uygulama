cded4c5bb97b1ca9c4277328ee63857047507b86
mrtozn1923 <mrtozn1923@gmail.com>
2021-03-23 22:24:50 +0300

    Lesson37 added

---
+++ 37-Page_Load_Animation/app.js
@@ -0,0 +1,6 @@
+var loadingContainer=document.querySelector(".loading-container");
+loadingContainer.style.display="flex";
+
+window.addEventListener("load",function(){
+    loadingContainer.style.display="none";
+});
