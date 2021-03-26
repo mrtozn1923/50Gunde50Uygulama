4aa5e4328e34d053b5a4de8e9c7dc168d6a14da5
mrtozn1923 <mrtozn1923@gmail.com>
2021-03-26 23:50:46 +0300

    app.js updated

--- 43-Form_With_Icons/app.js
+++ 43-Form_With_Icons/app.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
     var eye = document.getElementsByClassName("fa-eye")[0];
     var inputPassword=document.getElementById("password");
-    if (eye !== "undefined") {
+    if (eye !== undefined) {
         eye.addEventListener("click", function() {
             if (this.className === "far fa-eye") {
                 this.className = "far fa-eye-slash";
